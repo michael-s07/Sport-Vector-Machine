7e259e73528ded672305cf42e3e4e94bf5dbcb22
Michael S"exit()quit()git config --global user.name Michael <michael.sosu@amalitech.org>
2022-02-06 02:56:35 +0000

    Intial Commit

---
+++ script.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from svm_visualization import draw_boundary
+from players import aaron_judge, jose_altuve, david_ortiz
+
+fig, ax = plt.subplots()
